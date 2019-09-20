5ef443b214100333aec7288fdcdaabf2a4df3a18
CanadianMitch16 <mitchellniesar@gmail.com>
2019-09-20 12:10:07 -0400

    All files needed for Lab02 submission.

---
+++ Lab02/app/src/main/java/edu/calvin/cs262/mon2/MainActivity.java
@@ -0,0 +1,30 @@
+package edu.calvin.cs262.mon2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+
+    }
+}
+
+
+
